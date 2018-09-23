77cd2b932f6f4635fdbd6a0cae3257421a2f74a2
mwdowns <mwdowns@gmail.com>
2018-09-23 15:55:15 -0400

    working with conditionals

---
+++ conditionals/conditionals.js
@@ -0,0 +1,11 @@
+new Vue({
+  el: '#conditionals',
+  data: {
+    show: true,
+    things: ['spoon', 'light-bulb', 'stick of gum'],
+    animals: [
+      {name: 'Rovi', boy: 'pillow-boy'},
+      {name: 'Dani', boy: 'couch-boy'}
+    ]
+  }
+})
