2475f31b3648576684460d38e8a4665359177ad2
mwdowns <mwdowns@gmail.com>
2018-09-23 17:07:03 -0400

    tests for conditionals

---
+++ conditionals/test.js
@@ -0,0 +1,17 @@
+new Vue({
+  el: '#test',
+  data: {
+    show: true,
+    things: ['triskets', 'rubber-hose', 'humming-bird'],
+    myObject: {
+      title: 'Lord of the Rings',
+      author: 'J.R.R. Tolkiens',
+      books: '3'
+    },
+    testData: {
+      name: 'TESTOBJECT',
+      id: 10,
+      data: [1.67, 1.33, 0.98, 2.21]
+    }
+  }
+});
